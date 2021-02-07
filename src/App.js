6af89d669d0ebd6c651a6ac7e05072290e597014
Martin Petron <martinpetron@outlook.com>
2021-02-07 19:14:50 -0300

    79. Shop Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 
@@ -12,8 +13,8 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exaact path='/' component={HomePage} />
-        <Route exaact path='/hats' component={HomePage} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
