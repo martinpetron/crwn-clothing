d0982ea293b1dcd61b3eaf111578010e452d0c1b
Martin Petron <martinpetron@outlook.com>
2021-02-07 18:22:20 -0300

    77. withRouter()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+
+
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exaact path='/' component={HomePage} />
+        <Route exaact path='/hats' component={HomePage} />
+      </Switch>
     </div>
   );
 }
